3a05a8c54fd8e3b5e4bc93c90cd5050cf25e0c93
Mr Booi <ayabongabooi2@gmail.com>
2018-08-17 14:05:02 +0200

    Publish your code to a website

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const session = require('express-session');
-const  shoe  = require('./shoe_api.js');
+const Shoe = require('./shoe_api.js');
 const app = express();
 
 let PORT = process.env.PORT || 3000;
@@ -43,66 +43,156 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
-    helpers: {
-        checkedDays: function () {
-            if (this.checked) {
-                return 'checked';
-            }
-        }
-    }
+    defaultLayout: 'main'
+    // helpers: {
+    //     checkedDays: function () {
+    //         if (this.checked) {
+    //             return 'checked';
+    //         }
+    //     }
+    // }
 
 }));
 
 app.set('view engine', 'handlebars');
 
-const Shoe = shoe(pool);
+const shoe = Shoe(pool);
 
 app.get('/api/shoes', async (req, res) => {
-  try {
-    let shoes = await Shoe.allShoes();
-    res.json({
-         status:"success",
-        data:shoes});
+    try {
+        let shoes = await shoe.allShoes();
+        res.json({
+            status: "success",
+            data: shoes
+        });
 
-  } catch (e) {
-    res.json({
-        status:'error',
-      error:e.stack
-        
-    })
-  }
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+
+        })
+    }
 })
-app.get('/api/shoes/brand/:brandname', (req, res) => {
-    
+app.get('/api/shoes/brand/:brandname', async (req, res) => {
+    try {
+        const {
+            brandname
+        } = req.params;
+        if (brandname !== '' && brandname !== undefined) {
+            let searchByBrand = await shoe.findByBrand(brandname);
+            res.json({
+                status: "success",
+                data: searchByBrand
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+
+        })
+    }
 })
 app.get('/api/shoes/size/:size', (req, res) => {
-    res.json('Booi');
+    try {
+        const {
+            size
+        } = req.params;
+        if (size !== '' && size !== undefined) {
+            let searchBySize = await shoe.findBySize(size);
+            res.json({
+                status: "success",
+                data: searchBySize
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+
+        })
+    }
 })
-app.get('/api/shoes/brand/:brandname/size/:size', (req, res) => {
-    res.json('Shoe');
+app.get('/api/shoes/brand/:brandname/size/:size', async (req, res) => {
+    try {
+        const {
+            brandname,
+            size
+        } = req.params;
+        if (brandname !== '' && brandname !== undefined &&
+            size !== '' || size !== undefined) {
+            let searchByBrandAndSize = await shoe.findBybrandAndSize(brandname, size);
+            res.json({
+                status: "success",
+                data: searchByBrandAndSize
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+
+        })
+    }
 })
 
-app.post('/api/shoes/sold/:id', (req, res) => {
-    res.json('Api');
+app.post('/api/shoes/sold/:id', async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params;
+        if (id !== '' && id !== undefined) {
+            let addToCart = await shoe.addToShoppingCart(id);
+            res.json({
+                status: "success",
+                data: addToCart
+            })
+        } else {
+            return false;
+        }
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+        })
+    }
 })
 
-app.post('/api/shoes', (req, res) => {
-    res.json('shop');
+app.post('/api/shoes', async (req, res) => {
+    try {
+        const {
+            shoeBrand,
+            shoeColor,
+            shoeSize,
+            qty,
+            price
+        } = req.body;
+        if (shoeBrand !== '' && shoeSize !== '' && qty !== '' &&
+            price !== '' && shoeColor !== '') {
+            let addNewShoe = await shoe.addShoe(shoeBrand, shoeColor, shoeSize, price, qty);
+            res.json({
+                status: "success",
+                data: addNewShoe
+            })
+        } else {
+            return false
+        }
+
+    } catch (e) {
+        res.json({
+            status: 'error',
+            error: e.stack
+
+        })
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, (err) => {
     console.log('App starting on port', PORT)
