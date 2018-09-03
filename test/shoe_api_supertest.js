c9000b731eae363812be7d7ccf1ce51487c1062b
Mr Booi <ayabongabooi2@gmail.com>
2018-09-03 15:22:49 +0200

    fixing tests

--- test/shoe_api_supertest.js
+++ test/shoe_api_supertest.js
@@ -5,7 +5,7 @@ const baseURL = process.env.BASE_URL  ||'http://localhost:3000';
 
 
 describe('GET /api/shoes', function () {
-    it('respond with status of success if everything went well', (done) => {
+    it('respond with status of success if everything went well', () => {
         request(baseURL)
             .get('/api/shoes')
             .set('Accept', 'application/json')
@@ -14,13 +14,13 @@ describe('GET /api/shoes', function () {
             .then(result =>{
                 assert.deepEqual(result.body.status,'success');
             })
-            .end(done())
+            
     });
 });
 
 describe('post /api/shoes', function () {
   
-    it('should add shoe into a shopping list and return status of success', (done)=> {
+    it('should add shoe into a shopping list and return status of success', ()=> {
         let newShoe = {
             brand: 'Puma',
             color: 'black',
@@ -36,7 +36,7 @@ describe('post /api/shoes', function () {
             .then(result =>{
                 assert.equal(result.body.status,'success');
             })
-            .then(done())
+       
             
             
     });
@@ -44,7 +44,7 @@ describe('post /api/shoes', function () {
 
 describe('GET /api/shoes/brand/:brandname', function () {
    
-    it('should return status of success and brand name should be equal to Nike', function (done) {
+    it('should return status of success and brand name should be equal to Nike', function () {
         let brand ='Nike';
         request(baseURL)
             .get(`/api/shoes/brand/${brand}`)
@@ -54,12 +54,12 @@ describe('GET /api/shoes/brand/:brandname', function () {
                 assert.deepEqual(result.body.status,'success');
                 assert.deepEqual(result.body.data[0].brand,brand)
             })
-            .then(done())
+           
     });
 });
 
 describe('GET /api/shoes/size/:size',function () {
-    it('should return status of success and size equal to 8', function (done) {
+    it('should return status of success and size equal to 8', function () {
         let size =8;
         request(baseURL)
             .get(`/api/shoes/size/${size}`)
@@ -70,12 +70,12 @@ describe('GET /api/shoes/size/:size',function () {
                  assert.deepEqual(result.body.data[0].shoesize,size);
                  assert.deepEqual(result.body.data.length,2);
              })
-             .then(done())
+            
     });
 })
 
 describe('GET /api/shoes/brand/:brandname/size/:size',function () {
-    it('should return status of success, size equal to 8 and brand equal to Nike',(done)=> {
+    it('should return status of success, size equal to 8 and brand equal to Nike',()=> {
         let size =8;
         let brand = 'Nike'
         request(baseURL)
@@ -88,7 +88,7 @@ describe('GET /api/shoes/brand/:brandname/size/:size',function () {
                 assert.equal(result.body.data[0].shoesize,8);
                 assert.equal(result.body.data.length,1);
             })
-            .then(done())
+        
             
     });
 })
@@ -96,7 +96,7 @@ describe('GET /api/shoes/brand/:brandname/size/:size',function () {
  // Shopping cart
 
  describe('GET /api/view_cart',function () {
-    it('respond with json',  (done) =>{
+    it('respond with json',  () =>{
         request(baseURL)
             .get(`/api/view_cart`)
             .set('Accept', 'application/json')
@@ -104,12 +104,12 @@ describe('GET /api/shoes/brand/:brandname/size/:size',function () {
             .then(result=>{
             assert.equal(result.body.status,'success');
             })
-            .then(done())
+            
     });
 })
 
 describe('GET /api/cart/total',function () {
-    it('respond with json', function (done) {
+    it('respond with json', function () {
         request(baseURL)
             .get('/api/cart/total')
             .set('Accept', 'application/json')
@@ -117,12 +117,12 @@ describe('GET /api/cart/total',function () {
             .then(result=>{
             assert.equal(result.body.status,'success');
             })
-            .then(done())
+            
     });
 })
 
 describe('POST /api/shoes/cart/:id',function () {
-    it('respond with json', function (done) {
+    it('respond with json', function () {
         let id =3;
         let found = {status: 'success', data: true}
         request(baseURL)
@@ -132,13 +132,13 @@ describe('POST /api/shoes/cart/:id',function () {
             .then(result=>{
             assert.equal(result.body.status,'success');
             })
-            .then(done())
+            
     });
 })
 
 
 describe('GET /api/remove_cart',function () {
-    it('respond with json', function (done) {
+    it('respond with json', function () {
         let id =3;
         let found = {status: 'success', data: true}
         request(baseURL)
@@ -148,13 +148,13 @@ describe('GET /api/remove_cart',function () {
             .then(result=>{
             assert.equal(result.body.status,'success');
             })
-            .then(done())
+            
     });
 })
 
 
 describe('GET /api/remove_cart',function () {
-    it('respond with json', function (done) {
+    it('respond with json', function () {
         let id =3;
         let found = {status: 'success', data: true}
         request(baseURL)
@@ -164,7 +164,7 @@ describe('GET /api/remove_cart',function () {
             .then(result=>{
             assert.equal(result.body.status,'success');
             })
-            .then(done())
+            
     });
 })
 
